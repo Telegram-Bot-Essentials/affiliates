9a1c9f93d67acad917dbaa148d083402c63f9fa5
Elyar <elyareranjbar@gmail.com>
2025-10-18 22:49:48 +0330

    Affiliation models relations

--- src/Models/Affiliate.php
+++ src/Models/Affiliate.php
@@ -3,11 +3,24 @@
 namespace TelegramBotEssentials\Affiliates\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use TelegramBotEssentials\Essence\Models\BotUser;
 
 class Affiliate extends Model
 {
     use BelongsToTenant;
 
     protected $guarded = [];
+
+    public function botUser(): BelongsTo
+    {
+        return $this->belongsTo(BotUser::class);
+    }
+
+    public function referrals(): HasMany
+    {
+        return $this->hasMany(BotUser::class);
+    }
 }
--- src/Models/Referral.php
+++ src/Models/Referral.php
@@ -3,8 +3,23 @@
 namespace TelegramBotEssentials\Affiliates\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use TelegramBotEssentials\Essence\Models\BotUser;
 
 class Referral extends Model
 {
-    //
+    use BelongsToTenant;
+
+    protected $guarded = [];
+
+    public function botUser(): BelongsTo
+    {
+        return $this->belongsTo(BotUser::class);
+    }
+
+    public function affiliate(): BelongsTo
+    {
+        return $this->belongsTo(Affiliate::class);
+    }
 }
